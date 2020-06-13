0d41e1349140bb5b6f8753938975edf9862811c0
Romain Lenzotti <rom.lenzotti@gmail.com>
2020-06-13 19:07:54 +0200

    chore: Configure v6 branch

--- repo.config.js
+++ repo.config.js
@@ -16,51 +16,54 @@ module.exports = {
       author,
       contributors,
       license,
-      gitHead
+      gitHead,
+      publishConfig: {
+        "tag": "v6-alpha"
+      }
     });
 
     return json;
   },
   doc: {
     publish: true,
-    url: "https://github.com/TypedProject/tsed.git",
-    branch: "gh-pages",
-    cname: "tsed.io"
+    url: "https://github.com/TypedProject/v6.tsed.io.git",
+    branch: "master",
+    cname: "v6.tsed.io"
   },
   examples: {
-    "getting-started": {
-      url: "https://github.com/TypedProject/tsed-getting-started.git"
-    },
-    "aws": {
-      url: "https://github.com/TypedProject/tsed-example-aws.git"
-    },
-    "mongoose": {
-      url: "https://github.com/TypedProject/tsed-example-mongoose.git"
-    },
-    "multer": {
-      url: "https://github.com/TypedProject/tsed-example-multer.git"
-    },
-    "passportjs": {
-      url: "https://github.com/TypedProject/tsed-example-passportjs.git"
-    },
-    "typeorm": {
-      url: "https://github.com/TypedProject/tsed-example-typeorm.git"
-    },
-    "session": {
-      "url": "https://github.com/TypedProject/tsed-example-session.git"
-    },
-    "socketio": {
-      url: "https://github.com/TypedProject/tsed-example-socketio.git"
-    },
-    "passport-azure-ad": {
-      url: "https://github.com/TypedProject/tsed-example-passport-azure-ad.git"
-    },
-    "vuejs": {
-      url: "https://github.com/TypedProject/tsed-example-vuejs.git"
-    },
-    "react": {
-      url: "https://github.com/TypedProject/tsed-example-react.git"
-    }
+    // "getting-started": {
+    //   url: "https://github.com/TypedProject/tsed-getting-started.git"
+    // },
+    // "aws": {
+    //   url: "https://github.com/TypedProject/tsed-example-aws.git"
+    // },
+    // "mongoose": {
+    //   url: "https://github.com/TypedProject/tsed-example-mongoose.git"
+    // },
+    // "multer": {
+    //   url: "https://github.com/TypedProject/tsed-example-multer.git"
+    // },
+    // "passportjs": {
+    //   url: "https://github.com/TypedProject/tsed-example-passportjs.git"
+    // },
+    // "typeorm": {
+    //   url: "https://github.com/TypedProject/tsed-example-typeorm.git"
+    // },
+    // "session": {
+    //   "url": "https://github.com/TypedProject/tsed-example-session.git"
+    // },
+    // "socketio": {
+    //   url: "https://github.com/TypedProject/tsed-example-socketio.git"
+    // },
+    // "passport-azure-ad": {
+    //   url: "https://github.com/TypedProject/tsed-example-passport-azure-ad.git"
+    // },
+    // "vuejs": {
+    //   url: "https://github.com/TypedProject/tsed-example-vuejs.git"
+    // },
+    // "react": {
+    //   url: "https://github.com/TypedProject/tsed-example-react.git"
+    // }
   },
   tsdoc: require('./tsdoc.config.js')
 };
